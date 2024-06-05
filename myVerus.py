64f46d9efc2dd6fe22388568b032a585ae51a6d0
Tony <158815401+frogCaller@users.noreply.github.com>
2024-06-05 15:53:36 -0700

    Create myVerus.py

---
+++ myVerus.py
@@ -0,0 +1,155 @@
+## This script will give you the current date and time
+## and fetch your current verus stats. (current hashrate, current balance, total balance)
+## Feel free to modify the code to your likings! :) 
+
+## PLEASE ADD YOUR VERUS WALLET ON LINE 45:
+## my_Verus_Wallet = "YOUR_VERUS_WALLET"
+
+
+import time
+import random
+import sys
+import os
+import requests
+import logging
+from datetime import datetime
+import json
+import subprocess
+sys.path.append('/home/pi/python/screen/drive')
+from bs4 import BeautifulSoup
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'drive')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+import spidev
+from drive import SSD1305
+from PIL import Image,ImageDraw,ImageFont
+import re
+###################################################################
+
+disp = SSD1305.SSD1305()
+disp.Init()
+logging.info("clear display")
+disp.clear()
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+draw = ImageDraw.Draw(image)
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+padding = 0
+top = padding
+bottom = height-padding
+x = 0
+timeframe = 0.01
+font = ImageFont.truetype('04B_08__.TTF',8)
+
+my_Verus_Wallet = "YOUR_VERUS_WALLET"
+
+def convert_to_th(input_hashrate):
+    units = {
+        "H/s": 1e-12,
+        "KH/s": 1e-9,
+        "MH/s": 1e-6,
+        "GH/s": 1e-3,
+        "G H/s": 1e-3,
+        "TH/s": 1,
+        "PH/s": 1e3,
+        "EH/s": 1e6
+    }
+    try:
+        value, unit = input_hashrate.split()
+        return float(value) * units[unit]
+    except ValueError:
+        #print("Invalid input! Please enter a numerical value followed by units (e.g., 250 TH/s).")
+        #sys.exit(1)
+        return float(0.00)*units["H/s"]
+
+def get_coin_price(coin_id, coin_name):
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return data.get(coin_name, {}).get("usd")
+    except requests.RequestException as e:
+        print(f"Error fetching {coin_name} price: {e}")
+        return None
+
+def format_hash_rate(hash_rate):
+    units = ['H/s', 'KH/s', 'MH/s', 'GH/s', 'TH/s', 'PH/s', 'EH/s', 'ZH/s']
+    unit_index = 0
+    while hash_rate >= 1000 and unit_index < len(units) - 1:
+        hash_rate /= 1000
+        unit_index += 1
+    return f"{hash_rate:.2f} {units[unit_index]}"
+
+def format_with_commas(number):
+    return f"{number:,.2f}"
+
+def convert_to_output_units(hash_rate_th):
+    units = [
+        (1e9, "ZH/s"),
+        (1e6, "EH/s"),
+        (1e3, "PH/s"),
+        (1, "TH/s"),
+        (1e-3, "GH/s"),
+        (1e-6, "MH/s"),
+        (1e-9, "KH/s"),
+        (1e-12, "H/s")
+    ]
+    for factor, unit in units:
+        if hash_rate_th >= factor:
+            return hash_rate_th / factor, unit
+    return hash_rate_th, "H/s"
+
+
+def get_verus_data(verus_url):
+    try:
+        response = requests.get(verus_url)
+        response.raise_for_status()
+        data = response.json()
+        return {
+            "hashrate": data.get("hashrateString", "N/A"),
+            "balance": data.get("balance", "N/A"),
+            "paid": data.get("paid", "N/A")
+        }
+    except requests.RequestException as e:
+        print(f"Error fetching Verus data: {e}")
+        return None
+
+def buffer(sec):
+    disp.getbuffer(image)
+    disp.ShowImage()
+    time.sleep(sec)
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+def linetext(line, content):
+    draw.text((x, top+line), content, font=font, fill=255)
+
+def main():
+    verus_url = "https://luckpool.net/verus/miner/" + my_Verus_Wallet
+
+    current_date = datetime.now()
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    linetext(12, str(current_date.strftime("%B %d %Y  -  %-I:%M %p")))
+
+    buffer(3)
+
+    verus_data = get_verus_data(verus_url)
+    match = re.match(r"^\d+(\.\d+)?", verus_data['hashrate'])
+    hash_rate_num = float(match.group(0))
+    adjusted_hash_rate = hash_rate_num
+
+    if verus_data:
+        verus_price = get_coin_price("verus-coin", "verus-coin")
+        linetext(0, f"VRSC: ${verus_price:.2f}")
+        linetext(8, f"Amount: {verus_data['paid']:.3f}" + f" (${verus_price * verus_data['paid']:.2f})")
+        linetext(16, f"Now: {verus_data['balance']:.4f}" + f" (${verus_price * verus_data['balance']:.2f})")
+        linetext(24, f"RATE: {verus_data['hashrate']}/s")
+        buffer(20)
+
+        myVerus = convert_to_th(f"{verus_data['hashrate']}/s")
+
+
+if __name__ == "__main__":
+    while True:
+        main()
